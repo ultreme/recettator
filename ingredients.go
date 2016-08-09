b2b528d6a08d8b9b7b88523bb81b3a86d8c803db
Manfred Touron <m@42.am>
2016-08-09 08:42:24 +0200

    Initial code for ingredients

--- ingredients.go
+++ ingredients.go
@@ -1,5 +1,37 @@
 package recettator
 
-type Ingredient struct{}
+var RegisteredIngredients Ingredients
+
+type Ingredient interface {
+	Name() string
+	Kind() string
+	// Gender() string
+	// Quantity() string
+}
 
 type Ingredients []Ingredient
+
+type StandardIngredient struct {
+	name string
+	kind string
+
+	Gender   string
+	Multiple bool
+}
+
+func NewMainIngredient(name, gender string, multiple bool) StandardIngredient {
+	return StandardIngredient{
+		name: name,
+		kind: "main",
+
+		Gender:   gender,
+		Multiple: multiple,
+	}
+}
+
+func (i StandardIngredient) Name() string { return i.name }
+func (i StandardIngredient) Kind() string { return i.kind }
+
+func init() {
+	RegisteredIngredients = append(RegisteredIngredients, NewMainIngredient("jambon", "male", false))
+}
