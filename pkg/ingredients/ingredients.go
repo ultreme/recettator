b0283b6362a924e0a3902e86f3e99b853cf2dda3
Manfred Touron <m@42.am>
2016-09-07 19:04:54 +0200

    Update ingredients.go

--- pkg/ingredients/ingredients.go
+++ pkg/ingredients/ingredients.go
@@ -124,6 +124,7 @@ func NewPool(rnd *rand.Rand) *IngredientsPool {
 	pool.MainIngredients.append(NewMainIngredient("poney", "male", false))
 	pool.MainIngredients.append(NewMainIngredient("poulet", "male", false))
 	pool.MainIngredients.append(NewMainIngredient("requin", "male", false))
+	pool.MainIngredients.append(NewMainIngredient("sanglier", "male", false))
 	pool.MainIngredients.append(NewMainIngredient("saucisse", "female", false))
 	pool.MainIngredients.append(NewMainIngredient("saucisses Knacki®", "female", true))
 	pool.MainIngredients.append(NewMainIngredient("surimi", "male", false))
