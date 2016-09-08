2eb94d1666621947cbeffc528c0d03f7b4a16d64
Manfred Touron <m@42.am>
2016-09-08 15:22:13 +0200

    Update ingredients.go

--- pkg/ingredients/ingredients.go
+++ pkg/ingredients/ingredients.go
@@ -118,6 +118,7 @@ func NewPool(rnd *rand.Rand) *IngredientsPool {
 	pool.MainIngredients.append(NewMainIngredient("lardons", "male", true))
 	pool.MainIngredients.append(NewMainIngredient("lièvre", "male", false))
 	pool.MainIngredients.append(NewMainIngredient("lotte", "female", false))
+	pool.MainIngredients.append(NewMainIngredient("mouette", "female", false))
 	pool.MainIngredients.append(NewMainIngredient("nems", "male", true))
 	pool.MainIngredients.append(NewMainIngredient("oie", "female", false))
 	pool.MainIngredients.append(NewMainIngredient("pieuvre", "female", false))
