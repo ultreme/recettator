4520e97e7f7613e9e5d066ddb03f4b72c1521564
Manfred Touron <m@42.am>
2016-09-27 10:08:13 +0200

    Fix beginsWithVoyel to handle accents
    Signed-off-by: Manfred Touron <m@42.am>

--- pkg/ingredients/utils.go
+++ pkg/ingredients/utils.go
@@ -1,8 +1,20 @@
 package ingredients
 
+import (
+	"unicode"
+
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
 func beginsWithVoyel(word string) bool {
-	switch word[0] {
-	case 'â', 'a', 'e', 'i', 'o', 'u', 'y':
+	isMn := func(r rune) bool {
+		return unicode.Is(unicode.Mn, r)
+	}
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
+
+	switch result, _, _ := transform.String(t, word); result[0] {
+	case 'a', 'e', 'i', 'o', 'u', 'y':
 		return true
 	default:
 		return false
