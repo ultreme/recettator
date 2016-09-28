f81cf1f1279ebc18ec4c1cf53690099873d08581
Manfred Touron <m@42.am>
2016-09-28 10:42:54 +0200

    work in progress secondary ingredients list
    Signed-off-by: Manfred Touron <m@42.am>

--- pkg/ingredients/secondary_ingredients.go
+++ pkg/ingredients/secondary_ingredients.go
@@ -16,6 +16,7 @@ type SecondaryIngredient struct {
 	isSpice       bool
 	isByPiece     bool
 	isSpreadable  bool
+	rand          *rand.Rand
 }
 
 func NewSecondaryIngredient(name string, gender string, isMultiple bool, rnd *rand.Rand) *SecondaryIngredient {
@@ -23,6 +24,7 @@ func NewSecondaryIngredient(name string, gender string, isMultiple bool, rnd *ra
 		name:       name,
 		gender:     gender,
 		isMultiple: isMultiple,
+		rand:       rnd,
 		/*
 			isMultiple:
 			isUncountable:
@@ -33,13 +35,61 @@ func NewSecondaryIngredient(name string, gender string, isMultiple bool, rnd *ra
 			isSpreadable:
 		*/
 	}
-	// FIXME: compute quantity
 	return &ingredient
+}
+
+func (i *SecondaryIngredient) prepare() {
+	switch {
+	case i.isUncountable:
+		switch {
+		case i.isMultiple:
+			i.quantity = "des "
+			break
+		case beginsWithVoyel(i.name):
+			i.quantity = "de l'"
+			break
+		case i.gender == "male":
+			i.quantity = "du "
+			break
+		case i.gender == "female":
+			i.quantity = "de la "
+			break
+		}
+		break
+	case i.isPowder:
+		value := (i.rand.Intn(50) + 1) * 10
+		switch {
+		case value == 1 && !beginsWithVoyel(i.name):
+			i.quantity = fmt.Sprintf("%d gramme de ", value)
+			break
+		case value == 1 && beginsWithVoyel(i.name):
+			i.quantity = fmt.Sprintf("%d gramme d'", value)
+			break
+		case !beginsWithVoyel(i.name):
+			i.quantity = fmt.Sprintf("%d grammes de ", value)
+			break
+		case beginsWithVoyel(i.name):
+			i.quantity = fmt.Sprintf("%d grammes d'", value)
+			break
+		}
+		break
+	case i.isByPiece:
+		break
+	case i.isSpice:
+		break
+	case i.isSpreadable:
+		break
+	case i.isCitrus:
+		break
+	}
 }
 
 func (i SecondaryIngredient) Kind() string { return "secondary-ingredient" }
 func (i SecondaryIngredient) Name() string { return i.name }
 func (i SecondaryIngredient) NameAndQuantity() string {
+	if i.quantity == "" {
+		i.prepare()
+	}
 	return fmt.Sprintf("%s%s", i.quantity, i.name)
 }
 func (i SecondaryIngredient) GetGender() string { return i.gender }
